8be154ac9848bfff3e3ea561b1b3d011f14ad78c
Sorin Chis <kish.sorin@yahoo.com>
2019-06-25 20:57:30 +0300

    added Register screen style

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,17 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Register from "./components/Register";
+import { Route } from "react-router-dom";
+import PrivateRoute from "./utils/PrivateRoute";
+import Home from "./components/Home";
 
 function App() {
   return (
-    <div >
-
+    <div>
+      <div>
+        <Route exact path="/" component={Home} />
+        <Route path="/register" render={props => <Register {...props} />} />
+      </div>
     </div>
   );
 }
---
+++ client/src/components/Register.jsx
@@ -0,0 +1,104 @@
+import React, { useState } from "react";
+import { Input, Segment, Icon, Button, Dropdown } from "semantic-ui-react";
+import styled from "styled-components";
+import bg from "../assets/register.jpg";
+
+const Register = () => {
+  const [email, setEmail] = useState("");
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [role, setRole] = useState("student");
+
+  const dropDownOption = () => {
+    return [
+      {
+        key: "user",
+        text: (
+          <span>
+            <strong>Student</strong>
+          </span>
+        )
+      },
+      {
+        key: "avatar",
+        text: <span>Helper</span>
+      },
+      {
+        key: "signout",
+        text: <span>Admin</span>
+      }
+    ];
+  };
+  return (
+    <RegisterScreen>
+      <RegisterCard>
+        {/* <Icon name="registered" size="huge" bordered style={{ margin: 10 }} /> */}
+        <Form>
+          <InputWrapper
+            icon="users"
+            iconPosition="left"
+            placeholder="username"
+          />
+          <InputWrapper
+            icon="mail"
+            iconPosition="left"
+            placeholder="email"
+            type="mail"
+          />
+          <InputWrapper
+            icon="lock"
+            iconPosition="left"
+            placeholder="pasword"
+            type="password"
+          />
+          <Dropdown
+            button
+            className="icon"
+            floating
+            labeled
+            icon="world"
+            search
+            text="Select Role"
+            options={dropDownOption()}
+          />
+          <Button color="purple" size="small" style={{ width: 150 }}>
+            Register
+          </Button>
+        </Form>
+      </RegisterCard>
+    </RegisterScreen>
+  );
+};
+
+export default Register;
+
+const RegisterScreen = styled.div`
+  display: flex;
+  height: 100vh;
+  width: 100%;
+  justify-content: center;
+  align-items: center;
+  background-image: url(${bg});
+  background-repeat: no-repeat;
+  background-size: cover;
+`;
+
+const RegisterCard = styled.div`
+  height: 600px;
+  width: 600px;
+  border: 1px solid blue;
+  border-radius: 50%;
+  background: green;
+  opacity: 0.6;
+`;
+const Form = styled.form`
+  display: flex;
+  height: 100%;
+  flex-direction: column;
+  justify-content: space-evenly;
+  align-items: center;
+`;
+
+const InputWrapper = styled(Input)`
+  width: 300px;
+`;
