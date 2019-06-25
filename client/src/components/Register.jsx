e9474f4f1d91534c4ab9496d4efdc7114e66324f
Sorin Chis <kish.sorin@yahoo.com>
2019-06-25 20:59:29 +0300

    added effect when apear

--- client/src/components/Register.jsx
+++ client/src/components/Register.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { Input, Segment, Icon, Button, Dropdown } from "semantic-ui-react";
 import styled from "styled-components";
 import bg from "../assets/register.jpg";
+import Flip from "react-reveal/Flip";
 
 const Register = () => {
   const [email, setEmail] = useState("");
@@ -31,41 +32,43 @@ const Register = () => {
   };
   return (
     <RegisterScreen>
-      <RegisterCard>
-        {/* <Icon name="registered" size="huge" bordered style={{ margin: 10 }} /> */}
-        <Form>
-          <InputWrapper
-            icon="users"
-            iconPosition="left"
-            placeholder="username"
-          />
-          <InputWrapper
-            icon="mail"
-            iconPosition="left"
-            placeholder="email"
-            type="mail"
-          />
-          <InputWrapper
-            icon="lock"
-            iconPosition="left"
-            placeholder="pasword"
-            type="password"
-          />
-          <Dropdown
-            button
-            className="icon"
-            floating
-            labeled
-            icon="world"
-            search
-            text="Select Role"
-            options={dropDownOption()}
-          />
-          <Button color="purple" size="small" style={{ width: 150 }}>
-            Register
-          </Button>
-        </Form>
-      </RegisterCard>
+      <Flip left>
+        <RegisterCard>
+          {/* <Icon name="registered" size="huge" bordered style={{ margin: 10 }} /> */}
+          <Form>
+            <InputWrapper
+              icon="users"
+              iconPosition="left"
+              placeholder="username"
+            />
+            <InputWrapper
+              icon="mail"
+              iconPosition="left"
+              placeholder="email"
+              type="mail"
+            />
+            <InputWrapper
+              icon="lock"
+              iconPosition="left"
+              placeholder="pasword"
+              type="password"
+            />
+            <Dropdown
+              button
+              className="icon"
+              floating
+              labeled
+              icon="world"
+              search
+              text="Select Role"
+              options={dropDownOption()}
+            />
+            <Button color="purple" size="small" style={{ width: 150 }}>
+              Register
+            </Button>
+          </Form>
+        </RegisterCard>
+      </Flip>
     </RegisterScreen>
   );
 };
