19db2aefafae11a3fd2ff93d25bd4052f9fdd8b4
Sorin Chis <kish.sorin@yahoo.com>
2019-06-25 18:11:31 +0300

    added theme and modal for style

---
+++ client/src/theme/modalTheme.js
@@ -0,0 +1,59 @@
+import theme from "./styledTheme";
+
+const modalTheme = {
+  overlay: {
+    position: "fixed",
+    top: 0,
+    left: 0,
+    right: 0,
+    background: "rgba(0, 0, 0, 0.6)",
+    zIndex: 55,
+    width: "100%",
+    height: "100%",
+    bottom: 0
+  },
+  restrictedContent: {
+    position: "absolute",
+    top: "20%",
+    left: "50%",
+    transform: "translate(-50%, -50%)",
+    maxWidth: "100%",
+    width: "500px",
+    maxHeight: "100%",
+    height: "200px",
+    backgroundColor: theme.color.offWhite,
+    boxShadow: theme.shadow.cardShadow,
+    border: "none",
+    fontFamily: theme.font.fontFamily
+  },
+  translateContent: {
+    position: "absolute",
+    top: "35%",
+    left: "25%",
+    transform: "translate(-50%, -50%)",
+    maxWidth: "100%",
+    width: "500px",
+    maxHeight: "100%",
+    height: "300px",
+    margin: "1rem",
+    backgroundColor: theme.color.offWhite,
+    boxShadow: theme.shadow.cardShadow,
+    fontFamily: theme.font.fontFamily
+  },
+  paymentContent: {
+    position: "absolute",
+    top: "50%",
+    left: "50%",
+    right: "50%",
+    bottom: "50%",
+    transform: "translate(-50%, -50%)",
+    padding: "2rem",
+    width: "450px",
+    height: "40%",
+    backgroundColor: theme.color.offWhite,
+    boxShadow: theme.shadow.cardShadow,
+    fontFamily: theme.font.fontFamily
+  }
+};
+
+export default modalTheme;
---
+++ client/src/theme/styledTheme.js
@@ -0,0 +1,76 @@
+const theme = {
+  color: {
+    accentGreen: "#0CD4AF",
+    accentPurple: "#6876CB",
+    secondaryPurple: "#5B66AC",
+    hoverGreen: "#0bc1a0",
+    hoverPurple: "#5969c6",
+    lightGreen: "#e2fdf8",
+    lightPurple: "#F4F6FF",
+    textColor: "#333", //main text color
+    accentText: "#6876CB",
+    footerText: "#c7ccec",
+    white: "#FFFFFF",
+    offWhite: "#F2F2F2"
+  },
+
+  shadow: {
+    cardShadow: "0 16px 48px rgba(32, 41, 50, 0.21)",
+    containerShadow: "0 0 2px 0 rgba(0, 0, 0, 0.19)",
+    buttonHover: "0 3px 10px rgba(0, 0, 0, 0.15)",
+    buttonShadow: "0 0 3px 0 rgba(0, 0, 0, 0.19)"
+  },
+
+  font: {
+    fontFamily: "'Lato', sans-serif"
+  },
+
+  fontSize: {
+    xxxs: "0.875rem",
+    xxs: "1.2rem",
+    message: "1.4rem",
+    xs: "1.6rem",
+    s: "1.8rem",
+    sm: "2rem",
+    m: "2.4rem",
+    l: "3rem",
+    xl: "3.5rem"
+  },
+
+  fontStyle: {
+    italic: "italic"
+  },
+
+  textTransform: {
+    uppercase: "uppercase"
+  },
+
+  fontWeight: {
+    bold: "bold",
+    bolder: "bolder"
+  },
+
+  border: {
+    radius: "5px"
+  },
+
+  input: {
+    height: "40px"
+  },
+
+  button: {
+    height: "48px",
+    smallButton: "30px"
+  },
+
+  spinner: {
+    diameter: "30px"
+  },
+
+  spacing: {
+    lineHeight: "1.5",
+    letterSpacing: "0.1rem"
+  }
+};
+
+export default theme;
